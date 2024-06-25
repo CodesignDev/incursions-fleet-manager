69c846c57e0c792606b3f7874ed2513ed86862dc
Codesign <cs@csdev.io>
2024-06-25 01:49:02 +0100

    Add an info section to the queue stats that explains the position

--- resources/js/Pages/Waitlist/Partials/WaitlistQueueStats.tsx
+++ resources/js/Pages/Waitlist/Partials/WaitlistQueueStats.tsx
@@ -15,6 +15,37 @@ type WaitlistQueueStatsRowProps = {
     showWaitlistName?: boolean
 }
 
+function StatsInfo() {
+    const [showMoreInfo, toggleShowMoreInfo] = useToggle(false)
+
+    return (
+        <div className="flex flex-row gap-x-2 text-sm text-gray-600 dark:text-gray-400">
+            <div className="row-span-2">
+                <InformationCircleIcon className="size-5" />
+            </div>
+            <div className="flex flex-col items-start gap-y-1.5">
+                <p>
+                    Your queue position is only indicative of where you are on the waitlist, it does not indicate how
+                    long it will be before you are invited to the fleet.
+                </p>
+                <p className={tw({ hidden: !showMoreInfo })}>
+                    If you are unable to fly a ship that can fill the next slot that opens in the fleet, you may not be
+                    the next person to be invited, likewise if you are the only person on the waitlist in that ship and
+                    a a slot opens you will be the next person invited.
+                </p>
+
+                <button
+                    type="button"
+                    className="hover:text-gray-800 hover:underline focus:outline-none dark:hover:text-gray-200"
+                    onClick={() => toggleShowMoreInfo()}
+                >
+                    {showMoreInfo ? 'Less info...' : 'More info...'}
+                </button>
+            </div>
+        </div>
+    )
+}
+
 function StatsRow({ waitlist, showWaitlistName = false }: WaitlistQueueStatsRowProps) {
     const { name, on_waitlist: onWaitlist = false, queue_position: position = 0, total_entries: total = 0 } = waitlist
 
@@ -36,35 +67,15 @@ function StatsRow({ waitlist, showWaitlistName = false }: WaitlistQueueStatsRowP
 }
 
 export default function WaitlistQueueStats({ waitlists = [] }: WaitlistQueueStatsProps) {
-    const [showMoreInfo, toggleShowMoreInfo] = useToggle(false)
+    const onWaitlist = useMemo(() => waitlists.some(({ on_waitlist: queued }) => queued), [waitlists])
 
     return (
         <>
             <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">Waitlist Queue</h3>
-            <div className="flex flex-row gap-x-2 text-sm text-gray-600 dark:text-gray-400">
-                <div className="row-span-2">
-                    <InformationCircleIcon className="size-5" />
-                </div>
-                <div className="flex flex-col items-start gap-y-1.5">
-                    <p>
-                        Your queue position is only indicative of where you are on the waitlist, it does not indicate
-                        how long it will be before you are invited to the fleet.
-                    </p>
-                    <p className={tw({ hidden: !showMoreInfo })}>
-                        If you are unable to fly a ship that can fill the next slot that opens in the fleet, you may not
-                        be the next person to be invited, likewise if you are the only person on the waitlist in that
-                        ship and a a slot opens you will be the next person invited.
-                    </p>
-                    <button
-                        type="button"
-                        className="hover:text-gray-800 hover:underline focus:outline-none dark:hover:text-gray-200"
-                        onClick={() => toggleShowMoreInfo()}
-                    >
-                        {showMoreInfo ? 'Less info...' : 'More info...'}
-                    </button>
-                </div>
-            </div>
-            <div className="grid-rows-auto grid grid-cols-[min-content_1fr] gap-x-4 divide-y divide-gray-600">
+
+            {onWaitlist && <StatsInfo />}
+
+            <div className="grid grid-cols-[min-content_1fr] gap-x-4 divide-y divide-gray-600">
                 {match(waitlists.length, 2, {
                     0: <span>There are no active waitlists at this moment in time.</span>,
                     1: <StatsRow waitlist={waitlists[0]} />,
