09aac3822aaa5a0ba42038f67634c574a37de82e
Codesign <cs@csdev.io>
2024-12-25 12:04:01 +0000

    Update scheduler to use new loader jobs

--- routes/console.php
+++ routes/console.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Jobs\FetchAllianceStandings;
-use App\Jobs\FetchFleetMembers;
-use App\Jobs\FleetFleetInformation;
-use App\Jobs\SendPendingFleetInvites;
+use App\Jobs\FetchFleetInformationForAllFleets;
+use App\Jobs\FetchFleetMembersForAllFleets;
+use App\Jobs\SendAllPendingFleetInvites;
 use App\Models\Fleet;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
@@ -13,10 +13,14 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 // Schedule commands
-Schedule::job(new FetchAllianceStandings())->downtime()->onOneServer();
+Schedule::job(FetchAllianceStandings::class)->downtime()->onOneServer();
 
-// Fleet jobs
-$fleetExistsClosure = fn () => Fleet::whereTracked()->exists();
-Schedule::job(FleetFleetInformation::class)->everyMinute()->when($fleetExistsClosure); // Update fleet information
-Schedule::job(FetchFleetMembers::class)->everyTenSeconds()->when($fleetExistsClosure); // Update fleet members
-Schedule::job(SendPendingFleetInvites::class)->everyFiveSeconds()->when($fleetExistsClosure); // Send queued fleet invites
+// Tracked fleet jobs
+Schedule::when(fn () => Fleet::whereTracked()->exists())->group(function () {
+    Schedule::job(FetchFleetInformationForAllFleets::class)->everyMinute(); // Update fleet information
+    Schedule::job(FetchFleetMembersForAllFleets::class)->everyTenSeconds(); // Update fleet members
+    Schedule::job(SendAllPendingFleetInvites::class)->everyFiveSeconds(); // Send queued fleet invites
+});
+
+// Untracked fleet jobs
+Schedule::job(new FetchFleetInformationForAllFleets(includeUntracked: true))->everyFifteenMinutes()->when(fn () => Fleet::exists());
